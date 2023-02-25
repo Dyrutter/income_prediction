0bb5ee064e05583ad03582b7b9d46f738d6cbd9b
Dyrutter <126340978+Dyrutter@users.noreply.github.com>
2023-02-25 14:43:24 -0600

    Update test_app.py
    Cleaned up functions

--- App/test_app.py
+++ App/test_app.py
@@ -1,9 +1,9 @@
 from fastapi.testclient import TestClient
 from fastapi import status, FastAPI
-import requests
 import json
 from App.data import process_data, download, split
-from App.app import app ######Must import the app that was running in original file rather than create a new one, made issue with get
+from App.app import app 
+######Must import the app instantiated in original file rather than create a new one, made issue with get
 
 
 def test_download():
@@ -32,11 +32,11 @@ client = TestClient(app)
 
 def test_get_path():
     r = client.get("/")
-    assert r.status_code == status.HTTP_200_OK
+    assert r.status_code == status.HTTP_200_OK # Confirm active connection to server
     assert r.json() == {"Message": "This is a Salary Prediction Model API"}
 
 def test_over_50k():
-	data= {
+	example= {
 	"age": 26,
 	"workclass": "private",
 	"fnlgt": 189765,
@@ -51,15 +51,12 @@ def test_over_50k():
 	"capital-loss": 88,
 	"hours-per-week": 60,
 	"native-country": "United-States"}
-	r = client.post("/predict_salary", content=json.dumps(data)) #use content rather than data
-	#request = requests.post('http://127.0.0.1:8000/predict_salary', auth=('usr', 'pass'), data=json.dumps(data))
-	assert r.status_code == status.HTTP_200_OK #request.status_code == status.HTTP_200_OK
-	assert r.json() == {"salary": ">50K"} #request.json() == {"salary": ">50k"}
-	print(r.status_code)
-	print(r.json())
+	r = client.post("/predict_salary", content=json.dumps(example)) #use "content" argument rather than "data"
+	assert r.status_code == status.HTTP_200_OK 
+	assert r.json() == {"salary": ">50K"}
 
 def test_under_50k():
-	data={
+	example={
 	"age": 26,
 	"workclass": "private",
 	"fnlgt": 18976,
@@ -74,12 +71,9 @@ def test_under_50k():
 	"capital-loss": 0,
 	"hours-per-week": 0,
 	"native-country": "United-States"}
-	r = client.post("/predict_salary", content=json.dumps(data))
-	#request = requests.post('http://127.0.0.1:8000/predict_salary', auth=('usr', 'pass'), data=json.dumps(data))
+	r = client.post("/predict_salary", content=json.dumps(example)) # Dump JSON dict as string 
 	assert r.status_code == status.HTTP_200_OK
 	assert r.json() == {"salary": "<=50K"}
-	print(r.status_code)
-	print(r.json())
 
 if __name__ == '__main__':
 	test_download()
